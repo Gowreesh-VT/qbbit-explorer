41b11bda1c8ae9d741824a84e35e754251c284fe
Gowreesh V T <135145326+Gowreesh-VT@users.noreply.github.com>
2025-10-10 19:31:53 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <SimulationProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/qbbit-explorer">
+        <BrowserRouter>
           <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/theory" element={<Theory />} />
